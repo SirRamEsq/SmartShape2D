d1bad9c131782b5506e9b52cf1f7093817dd21ce
SirRamESQ <gmrl95@gmail.com>
2021-05-01 11:59:36 -0400

    Fix shape material test

--- gut/unit/test_shape_material.gd
+++ gut/unit/test_shape_material.gd
@@ -2,8 +2,11 @@ extends "res://addons/gut/test.gd"
 
 
 func test_edges_correct_ranges():
-	var nr1 = SS2D_NormalRange.new(-90.0, 90.0)
-	var nr2 = SS2D_NormalRange.new(91.0, -91.0)
+	# Two normals that don't overlap
+	# Right semicircle
+	var nr1 = SS2D_NormalRange.new(90.0, -180.0)
+	# Left semicircle
+	var nr2 = SS2D_NormalRange.new(91.0, 179.0)
 
 	var edge_mat_1 = SS2D_Material_Edge.new()
 	var edge_mat_2 = SS2D_Material_Edge.new()
@@ -19,32 +22,35 @@ func test_edges_correct_ranges():
 	shape_material.add_edge_material(edge_mat_meta_1)
 	shape_material.add_edge_material(edge_mat_meta_2)
 
+	# Vectors that match normal 1
 	var vectors_nr1 = [Vector2(1, 1), Vector2(1, 0), Vector2(1, -1)]
-	var vectors_nr2 = [Vector2(-1, 1), Vector2(-1, 0), Vector2(-1, -1)]
+	# Vectors that match normal 2
+	var vectors_nr2 = [Vector2(-1, 1), Vector2(-1, 0.1), Vector2(-1, -0.1)]
 
 	for v in vectors_nr1:
 		var materials = shape_material.get_edge_meta_materials(v)
-		assert_true(materials.size() > 0)
+		gut.p("Testing %s" % v)
+		assert_eq(materials.size(), 1, "Exactly one material returned")
 		if materials.size() > 0:
-			assert_eq(materials[0].edge_material, edge_mat_1)
+			assert_eq(materials[0].edge_material, edge_mat_1, "Matches material 1")
 
 	for v in vectors_nr2:
 		var materials = shape_material.get_edge_meta_materials(v)
-		assert_true(materials.size() > 0)
+		assert_eq(materials.size(), 1, "Exactly one material returned")
 		if materials.size() > 0:
-			assert_eq(materials[0].edge_material, edge_mat_2)
+			assert_eq(materials[0].edge_material, edge_mat_2, "Matches material 2")
 
 	for v in vectors_nr1:
 		var materials = shape_material.get_edge_meta_materials(v)
-		assert_true(materials.size() > 0)
+		assert_eq(materials.size(), 1, "Exactly one material returned")
 		if materials.size() > 0:
-			assert_ne(materials[0].edge_material, edge_mat_2)
+			assert_ne(materials[0].edge_material, edge_mat_2, "Doesn't match material 2")
 
 	for v in vectors_nr2:
 		var materials = shape_material.get_edge_meta_materials(v)
-		assert_true(materials.size() > 0)
+		assert_eq(materials.size(), 1, "Exactly one material returned")
 		if materials.size() > 0:
-			assert_ne(materials[0].edge_material, edge_mat_1)
+			assert_ne(materials[0].edge_material, edge_mat_1, "Doesn't match material 1")
 
 	var m = shape_material.get_all_edge_meta_materials()
 	assert_true(m[0] == edge_mat_meta_1)
