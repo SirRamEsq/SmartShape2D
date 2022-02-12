5fd5041be8746e70a31f50e89d5090b6d8962d58
Clay <clay.chinasky@gmail.com>
2022-02-12 20:26:12 +0300

    more similar fixes

--- addons/rmsmartshape/vertex_properties.gd
+++ addons/rmsmartshape/vertex_properties.gd
@@ -1,35 +1,41 @@
 @tool
-extends Resource
+extends RefCounted
 class_name SS2D_VertexProperties
 
 # @export (int) var texture_idx: int # setget set_texture_idx
-var texture_idx: int # setget set_texture_idx
+var _texture_idx: int # setget set_texture_idx
+@export var texture_idx: int:
+	get: return _texture_idx
+	set(v): set_texture_idx
 
 # @export (bool) var flip: bool #setget set_flip
-@export var flip: bool #setget set_flip
+var _flip: bool 
+@export var flip : bool:
+	get: return _flip 
+	set(v): set_flip
 
 # @export (float) var width: float #setget set_width
-@export var width: float #setget set_width
+var _width: float 
+@export var width: float:
+	get: return _width 
+	set(v): set_width
 
 
 func set_texture_idx(i: int):
 	texture_idx = i
 	emit_signal("changed")
-	# property_list_changed_notify()
 	notify_property_list_changed()
 
 
 func set_flip(b: bool):
 	flip = b
 	emit_signal("changed")
-	# property_list_changed_notify()
 	notify_property_list_changed()
 
 
 func set_width(w: float):
 	width = w
 	emit_signal("changed")
-	# property_list_changed_notify()
 	notify_property_list_changed()
 
 
