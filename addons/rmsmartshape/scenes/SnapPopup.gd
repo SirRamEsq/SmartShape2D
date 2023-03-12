f90a7d141aeab843f16e32847000f0f9098e50df
Narren <narren96c@pm.me>
2023-03-12 20:59:08 +0100

    Refactor SnapPopup

--- addons/rmsmartshape/scenes/SnapPopup.gd
+++ addons/rmsmartshape/scenes/SnapPopup.gd
@@ -1,15 +1,15 @@
 @tool
 extends Popup
 
-@export var p_snap_offset_x: NodePath
-@export var p_snap_offset_y: NodePath
-@export var p_snap_step_x: NodePath
-@export var p_snap_step_y: NodePath
+@onready var snap_offset_x: SpinBox = %SnapOffsetX
+@onready var snap_offset_y: SpinBox = %SnapOffsetY
+@onready var snap_step_x: SpinBox = %SnapStepX
+@onready var snap_step_y: SpinBox = %SnapStepY
 
 
 func get_snap_offset() -> Vector2:
-	return Vector2(get_node(p_snap_offset_x).value, get_node(p_snap_offset_y).value)
+	return Vector2(snap_offset_x.value, snap_offset_y.value)
 
 
 func get_snap_step() -> Vector2:
-	return Vector2(get_node(p_snap_step_x).value, get_node(p_snap_step_y).value)
+	return Vector2(snap_step_x.value, snap_step_y.value)
